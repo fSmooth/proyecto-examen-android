76c871f17c949388c5fdee1c06fb3eb030a5ce8c
fSmooth <fSmoothdev@gmail.com>
2017-02-20 18:16:05 +0100

    Implementada Actionbar

--- app/src/main/java/com/fsmooth/proyectoexamen/MainActivity.java
+++ app/src/main/java/com/fsmooth/proyectoexamen/MainActivity.java
@@ -5,10 +5,13 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +28,27 @@ public class MainActivity extends AppCompatActivity {
     private AdapterTrabajadores adapterTrabajadores;
 
     private List<Trabajadores> trabajadoresList;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_info:
+                Toast.makeText(this, "Alfonso Martínez Ponce", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_salir:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
